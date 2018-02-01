f7f37dc93c756f5e4977b97fd0c61ed23f2e88c4
thehomebrewnerd <4307001+thehomebrewnerd@users.noreply.github.com>
2018-02-01 09:16:05 -0600

    changed to relatitve time

---
+++ blogs/templatetags/filters.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from django.template.defaultfilters import timesince
+from django import template
+import pytz
+
+register = template.Library()
+
+
+@register.filter('ago')
+def ago(date_time):
+    diff = abs(date_time - datetime.utcnow().replace(tzinfo=pytz.utc))
+    if diff.days <= 0:
+        span = timesince(date_time)
+        span = span.split(",")[0] # just the most significant digit
+        return "{} ago".format(span)
+    else:
+        return "{} days ago".format(diff.days)
